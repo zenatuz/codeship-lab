0179b450fb208354579e8e7d1fda01d80d7caea8
Renato Batista <renato.batista@sigga.com>
2021-04-05 11:46:27 +0000

    Adicionando projeto básico em fastapi

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import sys
+import uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+uvicorn.run(app, host="0.0.0.0", port=8000)
